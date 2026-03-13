7849e44259d169dd37f1d78fd7cf1354f36a6b3d
New <218574791+Pranithi-New@users.noreply.github.com>
2026-03-13 15:40:47 +0700

    fix: update image paths in page component and enhance nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import { optimizeImage } from "next/dist/server/image-optimizer";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/ci-cd-test"
+  basePath: "/ci-cd-test",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/ci-cd-test/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -18,7 +18,7 @@ export default function Home() {
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://vercel.com/templates?framework=next.js"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -26,7 +26,7 @@ export default function Home() {
             </a>{" "}
             or the{" "}
             <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              href="https://nextjs.org/learn"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -38,13 +38,13 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://vercel.com/new"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src="/ci-cd-test/vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
@@ -53,7 +53,7 @@ export default function Home() {
           </a>
           <a
             className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs"
             target="_blank"
             rel="noopener noreferrer"
           >
